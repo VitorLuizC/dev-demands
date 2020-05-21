f91cca1cc09fba31151ae2e23a0d1dda938c48c0
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-05-21 03:13:17 -0300

    :sparkles: add express to server

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,36 +1,16 @@
-import { createServer } from 'http';
-import { parse } from 'querystring';
+import express from 'express';
 
-const server = createServer((request, response) => {
-  switch (request.url) {
-    case '/status': {
-      response.writeHead(200, {
-        'Content-Type': 'application/json',
-      });
-      response.write(
-        JSON.stringify({
-          status: 'Okay',
-        })
-      );
-      response.end();
-      break;
-    }
-    case '/authenticate': {
-      let data = '';
-      request.on('data', (chunk) => {
-        data += chunk;
-      });
-      request.on('end', () => {
-        const params = parse(data);
-        response.end();
-      });
-      break;
-    }
-    default: {
-      response.writeHead(404, 'Service not found.');
-      response.end();
-    }
-  }
+const server = express();
+
+server.get('/status', (_, response) => {
+  response.send({
+    status: 'Okay',
+  });
+});
+
+server.post('/authenticate', express.json(), (request, response) => {
+  console.log('E-Mail', request.body.email, 'Senha', request.body.password);
+  response.send();
 });
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
