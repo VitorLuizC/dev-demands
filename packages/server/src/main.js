9225f4f74a5fb176f651536ceb804305cb2f8cec
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-05-22 01:52:48 -0300

    :sparkles: add nodemon and apollo-server

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,28 +1,59 @@
 import express from 'express';
-import cors from 'cors';
 
-const server = express();
+import { ApolloServer, gql } from 'apollo-server-express';
 
-server.get('/status', (_, response) => {
-  response.send({
-    status: 'Okay',
-  });
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Client {
+      id: ID!
+      name: String!
+    }
+
+    type Demand {
+      id: ID!
+      name: String!
+      client: Client!
+      deadline: String
+    }
+
+    type Query {
+      demands: [Demand]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      demands: () => [],
+    },
+  },
 });
 
-const enableCors = cors({ origin: 'http://localhost:3000' });
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
 
-server
-  .options('/authenticate', enableCors)
-  .post('/authenticate', enableCors, express.json(), (request, response) => {
-    console.log('E-Mail', request.body.email, 'Senha', request.body.password);
-    response.send({
-      Okay: true,
-    });
-  });
+// server.get('/status', (_, response) => {
+//   response.send({
+//     status: 'Okay',
+//   });
+// });
+
+// server
+//   .options('/authenticate', enableCors)
+//   .post('/authenticate', enableCors, express.json(), (request, response) => {
+//     console.log('E-Mail', request.body.email, 'Senha', request.body.password);
+//     response.send({
+//       Okay: true,
+//     });
+//   });
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 
-server.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
 });
