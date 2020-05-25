470b4a251f1124d43a623190f05ba87a62d5ed2a
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-05-25 01:02:54 -0300

    :sparkles: add update, delete, disable and enable client mutations

--- packages/server/src/graphql/Client/Client.js
+++ packages/server/src/graphql/Client/Client.js
@@ -42,8 +42,18 @@ export const typeDefs = gql`
     email: String!
   }
 
+  input UpdateClientInput {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
   extend type Mutation {
     createClient(input: CreateClientInput!): Client!
+    updateClient(input: UpdateClientInput!): Client!
+    deleteClient(id: ID!): Client!
+    enableClient(id: ID!): Client!
+    disableClient(id: ID!): Client!
   }
 `;
 
@@ -123,6 +133,94 @@ export const resolvers = {
       await clientRepository.write([...clients, client]);
 
       return client;
+    },
+
+    updateClient: async (_, { input }) => {
+      const clients = await clientRepository.read();
+
+      const currentClient = clients.find((client) => client.id === input.id);
+
+      if (!currentClient)
+        throw new Error(`No client with this id "${input.id}"`);
+
+      const updatedClient = {
+        ...currentClient,
+        name: input.name,
+        email: input.email,
+      };
+
+      const updatedClients = clients.map((client) => {
+        if (client.id === updatedClient.id) return updatedClient;
+        return client;
+      });
+
+      await clientRepository.write(updatedClients);
+
+      return updatedClient;
+    },
+
+    deleteClient: async (_, { id }) => {
+      const clients = await clientRepository.read();
+
+      const client = clients.find((client) => client.id === id);
+
+      if (!client) throw new Error(`Cannot delete client with id "${id}"`);
+
+      const updatedClients = clients.filter((client) => client.id !== id);
+
+      await clientRepository.write(updatedClients);
+
+      return client;
+    },
+
+    enableClient: async (_, { id }) => {
+      const clients = await clientRepository.read();
+
+      const currentClient = clients.find((client) => client.id === id);
+
+      if (!currentClient) throw new Error(`No client with this id "${id}"`);
+
+      if (!currentClient.disabled)
+        throw new Error(`Client "${id}" is already enabled.`);
+
+      const updatedClient = {
+        ...currentClient,
+        disabled: false,
+      };
+
+      const updatedClients = clients.map((client) => {
+        if (client.id === updatedClient.id) return updatedClient;
+        return client;
+      });
+
+      await clientRepository.write(updatedClients);
+
+      return updatedClient;
+    },
+
+    disableClient: async (_, { id }) => {
+      const clients = await clientRepository.read();
+
+      const currentClient = clients.find((client) => client.id === id);
+
+      if (!currentClient) throw new Error(`No client with this id "${id}"`);
+
+      if (currentClient.disabled)
+        throw new Error(`Client "${id}" is already disabled.`);
+
+      const updatedClient = {
+        ...currentClient,
+        disabled: true,
+      };
+
+      const updatedClients = clients.map((client) => {
+        if (client.id === updatedClient.id) return updatedClient;
+        return client;
+      });
+
+      await clientRepository.write(updatedClients);
+
+      return updatedClient;
     },
   },
 };
